d74f252254e38c131eed523522c14079120713e6
tusharsaurabh <tusharacc@gmail.com>
2025-06-24 21:52:06 +0530

    Initial Commit

---
+++ dashcam/dashcamApp.swift
@@ -0,0 +1,17 @@
+//
+//  dashcamApp.swift
+//  dashcam
+//
+//  Created by Tushar Saurabh on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct dashcamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dashcamTests/dashcamTests.swift
@@ -0,0 +1,17 @@
+//
+//  dashcamTests.swift
+//  dashcamTests
+//
+//  Created by Tushar Saurabh on 6/24/25.
+//
+
+import Testing
+@testable import dashcam
+
+struct dashcamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
